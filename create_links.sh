0f18d408f9a504bff5507993e74f5833d924c7e8
take-cheeze <takechi101010@gmail.com>
2018-06-14 05:53:48 +0000

    Add eww-bookmarks.

--- create_links.sh
+++ create_links.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd $(dirname $0)
-for i in bashrc emacs.el gitconfig gitignore; do
+for i in bashrc emacs.el gitconfig gitignore emacs.d/eww-bookmarks; do
     rm -f ~/.$i
     ln -s $PWD/.$i ~/.$i
 done
