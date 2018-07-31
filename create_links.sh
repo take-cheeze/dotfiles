941f97e32f58b363233c6e669e8b57ba7005e53a
take-cheeze <takechi101010@gmail.com>
2018-08-01 01:00:49 +0900

    Add gemrc.

--- create_links.sh
+++ create_links.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd $(dirname $0)
-for i in bashrc emacs.el gitconfig gitignore emacs.d/eww-bookmarks tmux.conf; do
+for i in bashrc emacs.el gitconfig gitignore emacs.d/eww-bookmarks tmux.conf gemrc; do
     rm -f ~/.$i
     echo "linking: $i"
     ln -s $PWD/.$i ~/.$i
