491a95d0713d358c4bfe4b5e565900391be315b1
take-cheeze <takechi101010@gmail.com>
2018-08-02 00:36:49 +0900

    Add paren configs.

--- .emacs.el
+++ .emacs.el
@@ -189,6 +189,10 @@
 ;; vc-mode
 (global-set-key (kbd "C-c g g") #'vc-git-grep)
 
+;; parenthsis
+(show-paren-mode 1)
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -204,10 +208,10 @@
                   company-oddmuse company-dabbrev company-abbrev company-ansible company-inf-ruby company-bibtex company-c-headers company-dict company-emoji company-go company-lua company-math-symbols-latex company-math-symbols-unicode
                   (company-shell company-shell-env company-fish-shell)
                   company-web-html company-web-jade company-web-slim company-terraform)))
- '(company-dabbrev-code-modes t)
  '(company-dabbrev-code-everywhere t)
- '(company-dabbrev-ignore-case nil)
+ '(company-dabbrev-code-modes t)
  '(company-dabbrev-downcase nil)
+ '(company-dabbrev-ignore-case nil)
  '(company-idle-delay 0.2)
  '(compilation-scroll-output t)
  '(desktop-restore-frames t)
@@ -222,7 +226,7 @@
  '(next-screen-context-lines 3)
  '(package-selected-packages
    (quote
-    (rubocopfmt dockerfile-mode nginx-mode use-package flycheck-elm twittering-mode company helm-git-grep cider helm web-mode typescript-mode elm-mode wandbox xterm-color editorconfig apache-mode tablist ruby-mode magit inf-ruby haskell-mode gh emmet-mode auto-complete yari yaml-mode wgrep undohist undo-tree toml-mode switch-window smart-cursor-color sane-term rust-mode ruby-electric php-mode pdf-tools pbcopy paredit org-ac open-junk-file multi-term minibuf-isearch milkode magit-gitflow magit-gh-pulls magit-filenotify lua-mode json-mode js2-mode highlight-indentation google-c-style go-mode glsl-mode git-gutter git-blamed gist ghc flycheck-rust flycheck-haskell express dtrt-indent d-mode csv cssh coffee-mode cmake-mode clang-format alert company-ansible company-bibtex company-c-headers company-dict company-emoji company-glsl company-go company-inf-ruby company-lua company-math company-nginx company-quickhelp company-shell company-terraform company-web)))
+    (rainbow-delimiters rubocopfmt dockerfile-mode nginx-mode use-package flycheck-elm twittering-mode company helm-git-grep cider helm web-mode typescript-mode elm-mode wandbox xterm-color editorconfig apache-mode tablist ruby-mode magit inf-ruby haskell-mode gh emmet-mode auto-complete yari yaml-mode wgrep undohist undo-tree toml-mode switch-window smart-cursor-color sane-term rust-mode ruby-electric php-mode pdf-tools pbcopy paredit org-ac open-junk-file multi-term minibuf-isearch milkode magit-gitflow magit-gh-pulls magit-filenotify lua-mode json-mode js2-mode highlight-indentation google-c-style go-mode glsl-mode git-gutter git-blamed gist ghc flycheck-rust flycheck-haskell express dtrt-indent d-mode csv cssh coffee-mode cmake-mode clang-format alert company-ansible company-bibtex company-c-headers company-dict company-emoji company-glsl company-go company-inf-ruby company-lua company-math company-nginx company-quickhelp company-shell company-terraform company-web)))
  '(read-buffer-completion-ignore-case nil)
  '(read-file-name-completion-ignore-case nil)
  '(ruby-indent-level 2)
@@ -380,3 +384,7 @@
   :config
   (eval-after-load 'nginx-mode
     '(add-hook 'nginx-mode-hook #'company-nginx-keywords)))
+
+(use-package rainbow-delimiters
+  :config
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
