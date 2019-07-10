e71264105b8b85489266e1c5b1b564c35712e120
take-cheeze <takechi101010@gmail.com>
2019-07-10 13:13:30 +0900

    Create ~/.emacs.d

--- create_links.sh
+++ create_links.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cd $(dirname $0)
+mkdir -p ~/.emacs.d
 for i in bashrc emacs.el gitconfig gitignore emacs.d/eww-bookmarks tmux.conf gemrc bazelrc pythonrc.py gdbinit; do
     rm -f ~/.$i
     echo "linking: $i"
