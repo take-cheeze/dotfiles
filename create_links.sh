e3bb24b573a44b8dc4803beb3f5c86d63102ebf0
take-cheeze <takechi101010@gmail.com>
2019-12-27 16:05:07 +0900

    Add direnvrc

--- create_links.sh
+++ create_links.sh
@@ -2,8 +2,9 @@
 
 cd $(dirname $0)
 mkdir -p ~/.emacs.d
-for i in bashrc emacs.el gitconfig gitignore emacs.d/eww-bookmarks tmux.conf gemrc bazelrc pythonrc.py gdbinit wl folders; do
+for i in bashrc emacs.el gitconfig gitignore emacs.d/eww-bookmarks tmux.conf gemrc bazelrc pythonrc.py gdbinit wl folders config/direnv/direnvrc; do
     rm -f ~/.$i
+    mkdir -p $(dirname ~/.$i)
     echo "linking: $i"
     ln -s "$PWD/.$i" ~/.$i
 done
