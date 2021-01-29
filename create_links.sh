bbed4041f375ce19c75c46ebf8afca1e3bec9a5c
take-cheeze <takechi101010@gmail.com>
2021-01-29 13:29:17 +0900

    Sort link targets

--- create_links.sh
+++ create_links.sh
@@ -4,7 +4,28 @@ set -e
 
 cd "$(dirname "$0")"
 mkdir -p ~/.emacs.d
-for i in bashrc bash_aliases emacs.el gitconfig gitignore emacs.d/eww-bookmarks tmux.conf gemrc bazelrc pythonrc.py gdbinit wl folders config/direnv/direnvrc Xmodmap xprofile ssh/rc profile zshrc brew-aliases; do
+for i in \
+    Xmodmap \
+    bash_aliases \
+    bashrc \
+    bazelrc \
+    brew-aliases \
+    config/direnv/direnvrc \
+    emacs.d/eww-bookmarks \
+    emacs.el \
+    folders \
+    gdbinit \
+    gemrc \
+    gitconfig \
+    gitignore \
+    profile \
+    pythonrc.py \
+    ssh/rc \
+    tmux.conf \
+    wl \
+    xprofile \
+    zshrc \
+    ; do
     rm -f ~/.$i
     mkdir -p "$(dirname ~/.$i)"
     echo "linking: $i"
