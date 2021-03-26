65201a62bd1a8374e6bff7782f19c6e8e607565f
take-cheeze <takechi101010@gmail.com>
2021-03-27 01:05:32 +0900

    Use https

--- .emacs.el
+++ .emacs.el
@@ -12,7 +12,7 @@
 ;; You may delete these explanatory comments.
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 (package-initialize)
 (package-refresh-contents)
 
