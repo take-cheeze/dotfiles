903e523aa548935c78d96bc4c8bf53fe310c488a
take-cheeze <takechi101010@gmail.com>
2025-04-05 17:07:55 +0900

    Fix setup script

--- setup-home.bash
+++ setup-home.bash
@@ -6,16 +6,19 @@ cd $(dirname $0)
 
 command -v nix || sh <(curl -L https://nixos.org/nix/install) --daemon
 
+./create_links.sh
+rm -f ~/.bashrc ~/.profile
+
 if ! command -v home-manager ; then
     nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz home-manager
     nix-channel --update
 
     nix-shell '<home-manager>' -A install
 
+    set +u
     . $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
+    set -u
 fi
 
-./create_links.sh
-rm -f ~/.bashrc ~/.profile
 home-manager switch
 ./create_links.sh 
